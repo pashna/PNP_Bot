26779a32160c1b639de18ddef406fe2997643a63
Pashna <p02p@yandex.ru>
2015-12-30 20:47:55 +0300

    While True

--- data_collector.py
+++ data_collector.py
@@ -10,17 +10,22 @@ from Config import GET_FIRST_TIME
 
 if __name__ == '__main__':
 
-    db = DB()
-    engine = Engine(GET_FIRST_TIME())
+    while (1):
+        try:
+            db = DB()
+            engine = Engine(GET_FIRST_TIME())
 
-    while(1):
-        predicted = engine.predict()
-        print "{}:    predicted = {}".format(datetime.today(), predicted)
-        for pred in predicted:
-            # Если новость есть
-            if pred[0] != "":
-                db.insert_news(pred[0], pred[1])
+            while(1):
+                predicted = engine.predict()
+                print "{}:    predicted = {}".format(datetime.today(), predicted)
+                for pred in predicted:
+                    # Если новость есть
+                    if pred[0] != "":
+                        db.insert_news(pred[0], pred[1])
 
-        sleep_time = engine.syncClock()
-        print "Спим {} секунд".format(sleep_time)
-        time.sleep(sleep_time)
+                sleep_time = engine.syncClock()
+                print "Спим {} секунд".format(sleep_time)
+                time.sleep(sleep_time)
+
+        except Exception as e:
+            print e
--- telegram_bot.py
+++ telegram_bot.py
@@ -18,24 +18,30 @@ def get_news_type(news):
 
 if __name__ == '__main__':
 
-    db = DB()
-    chats = Chats(db)
-    bot = Bot(db, chats)
+    while 1:
+        try:
+            db = DB()
+            chats = Chats(db)
+            bot = Bot(db, chats)
+            time.sleep(120)
 
-    while(1):
+            while(1):
 
-        date = datetime.today() + timedelta(minutes=1)
+                date = datetime.today() + timedelta(minutes=1)
 
-        new_news = db.get_new_news()
-        for news, predicted in new_news:
+                new_news = db.get_new_news()
+                for news, predicted in new_news:
 
-            news_type = get_news_type(news)
-            chats_list = chats.get_relevant_chats(news_type, predicted)
-            bot.send_message(chats_list, news, predicted)
+                    news_type = get_news_type(news)
+                    chats_list = chats.get_relevant_chats(news_type, predicted)
+                    bot.send_message(chats_list, news, predicted)
 
-            db.mark_news_as_sent(news)
+                    db.mark_news_as_sent(news)
 
-        time_to_sleep = int ((date - datetime.today()).total_seconds())
+                time_to_sleep = int ((date - datetime.today()).total_seconds())
 
-        if (time_to_sleep > 0):
-            time.sleep(time_to_sleep)
+                if (time_to_sleep > 0):
+                    time.sleep(time_to_sleep)
+
+        except Exception as e:
+            print e
